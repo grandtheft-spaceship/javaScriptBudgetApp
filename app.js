a93161edb88b12eb325b154a9c3877b0a64fa7be
grandtheft-spaceship <justdizon71@gmail.com>
2017-07-03 15:18:32 -0700

    :art: Adds functionality to calculate budeget, totals, and percentage for incomes/expenses

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var budgetController = ( function() {
     this.value = value;
   };
 
+  var calculateTotal = function( type ) {
+    var sum = 0;
+
+    data.allItems[ type ].forEach( function( cur ) {
+      sum += cur.value;
+    });
+    data.totals[ type ] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -20,7 +29,9 @@ var budgetController = ( function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -43,12 +54,33 @@ var budgetController = ( function() {
       // Return the new item
       return newItem;
     },
+    calculateBudget: function() {
+      // 1. Calculate total incomes and expenses
+      calculateTotal( 'exp' );
+      calculateTotal( 'inc' );
+
+      // 2. Calculate the budget ( income - expenses )
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // 3. Calculate percentage of income that is already spent
+      if ( data.totals.inc > 0 ) {
+        data.percentage = Math.round( ( data.totals.exp / data.totals.inc )* 100 );
+      } else {
+        data.percentage = -1;
+      }
+    },
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalIncome: data.totals.inc,
+        totalExpenses: data.totals.exp,
+        percentage: data.percentage
+      };
+    },
     testing: function() {
       console.log( data );
     }
   }
-
-
 })();
 ////////////////////////////////////////////////////////////
 
@@ -132,10 +164,12 @@ var controller = ( function( budgetCtrl, UICtrl ) {
 
   var updateBudget = function() {
     // 1. Calculate the budget
-
+    budgetCtrl.calculateBudget();
     // 2. Return the budget
-
+    var budget = budgetCtrl.getBudget();
     // 3. Display the budget on the UI
+    console.log(budget);
+
   };
 
   var ctrlAddItem = function() {
