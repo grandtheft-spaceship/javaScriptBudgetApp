fa480d2e38c408c038b134769563d8e479ff1f8b
grandtheft-spaceship <justdizon71@gmail.com>
2017-07-03 17:06:52 -0700

    :art: Adds event handler for delete action

--- app.js
+++ app.js
@@ -85,6 +85,15 @@ var budgetController = ( function() {
 ////////////////////////////////////////////////////////////
 
 
+
+
+
+
+
+
+
+
+
 ////////////////////////////////////////////////////////////
 var UIController = ( function() {
   var DOMstrings = {
@@ -97,7 +106,8 @@ var UIController = ( function() {
       budgetLabel: ".budget__value",
       incomeLabel: ".budget__income--value",
       expensesLabel: ".budget__expenses--value",
-      percentageLabel: ".budget__expenses--percentage"
+      percentageLabel: ".budget__expenses--percentage",
+      container: ".container"
   };
 
   return  {
@@ -118,11 +128,11 @@ var UIController = ( function() {
       if ( type === "inc" ) {
         element = DOMstrings.incomeContainer;
 
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
       } else if ( type === "exp" ) {
         element = DOMstrings.expensesContainer;
 
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
       }
 
       // 2. Replace placeholder text with actual data from the object
@@ -164,6 +174,15 @@ var UIController = ( function() {
 ////////////////////////////////////////////////////////////
 
 
+
+
+
+
+
+
+
+
+
 ////////////////////////////////////////////////////////////
 var controller = ( function( budgetCtrl, UICtrl ) {
   var setupEventListeners = function() {
@@ -176,6 +195,9 @@ var controller = ( function( budgetCtrl, UICtrl ) {
         ctrlAddItem();
       }
     });
+
+    document.querySelector( DOM.container ).addEventListener( "click", ctrlDeleteItem );
+
   };
 
   var updateBudget = function() {
@@ -206,6 +228,22 @@ var controller = ( function( budgetCtrl, UICtrl ) {
 
       // 5. Calculate and update budget
       updateBudget();
+    }
+  };
+
+  var ctrlDeleteItem = function( event ) {
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if ( itemID ) {
+      splitID = itemID.split("-");
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. Delete item from data structure
+      // 2. Delete item from UI
+      // 3. Update and show the new budget
     }
   };
 
