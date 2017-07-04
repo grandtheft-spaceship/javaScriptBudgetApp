c1badba4a7190e2facf10f624c919e0c825db08e
grandtheft-spaceship <justdizon71@gmail.com>
2017-07-04 05:48:21 -0700

    :art: Adds month and year to budget title:

--- app.js
+++ app.js
@@ -146,7 +146,8 @@ var UIController = ( function() {
       expensesLabel: ".budget__expenses--value",
       percentageLabel: ".budget__expenses--percentage",
       container: ".container",
-      expensesPercentageLabel: ".item__percentage"
+      expensesPercentageLabel: ".item__percentage",
+      dateLabel: ".budget__title--month"
   };
 
   var formatNumber = function( number, type ) {
@@ -256,6 +257,15 @@ var UIController = ( function() {
         }
 
       });
+    },
+    displayMonth: function() {
+      var now, year, month, months;
+      months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      now = new Date();
+      month = now.getMonth();
+      year = now.getFullYear();
+
+      document.querySelector( DOMstrings.dateLabel ).textContent = months[ month ] + " " + year;
     }
 
   };
@@ -358,6 +368,7 @@ var controller = ( function( budgetCtrl, UICtrl ) {
   return {
     init: function() {
       console.log("APP IS RUNNING");
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
