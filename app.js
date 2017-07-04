fd1d16daaabf5a22ad282be982f8e0751667a2ed
grandtheft-spaceship <justdizon71@gmail.com>
2017-07-03 20:23:51 -0700

    :art: Sets up updatePercentages function

--- app.js
+++ app.js
@@ -228,6 +228,15 @@ var controller = ( function( budgetCtrl, UICtrl ) {
 
   };
 
+  var updatePercentages = function() {
+    // 1. Calculate the percentages
+
+    // 2. Read percentages from the budget controller
+
+    // 3. Update the UI with the new percentages
+
+  }
+
   var ctrlAddItem = function() {
     var input, newItem;
 
@@ -246,6 +255,8 @@ var controller = ( function( budgetCtrl, UICtrl ) {
 
       // 5. Calculate and update budget
       updateBudget();
+
+      // 6. Calculate and update percentages
     }
   };
 
@@ -265,6 +276,7 @@ var controller = ( function( budgetCtrl, UICtrl ) {
       UICtrl.deleteListItem( itemID );
       // 3. Update and show the new budget
       updateBudget();
+      // 4. Calculate and update percentages
 
     }
   };
